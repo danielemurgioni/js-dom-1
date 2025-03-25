2e1e24545e1666d78bd51d5129263ef43939b23f
danielemurgioni <dashomew@gmail.com>
2025-03-25 14:31:23 +0100

    [UPD] Created function for button on-off

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // inizializzo le variabili della lampada e del bottone
 const lamp = document.getElementById("lamp");
 const buttonSwitch = document.getElementById("btn-on-off");
+
+// creo la funzione per il bottone in modo che venga
+buttonSwitch.addEventListener("click", function(){
+    if(lamp.src.includes("yellow")){
+        lamp.src = "./img/white_lamp.png";
+        buttonSwitch.textContent = "Accendi";
+    }
+    else{
+        lamp.src = "./img/yellow_lamp.png";
+        buttonSwitch.textContent = "Spegni";
+    }
+})
